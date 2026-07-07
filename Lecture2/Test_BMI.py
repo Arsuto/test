458190bab3cd6ca21da7bf574e79a2591f339541
Arsuto <arsuto48395@gmail.com>
2026-07-07 15:26:01 +0700

    Lecture2

---
+++ Lecture2/Test_BMI.py
@@ -0,0 +1,5 @@
+
+weigh = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
+bmi = weigh / (height * height)
+print('Your BMI is', format(bmi, ".2f"))
---
+++ Lecture2/test_practical.py
@@ -0,0 +1,3 @@
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+print("Your are " + str(age) + " Years old and " + str(height) + " cm. tall.")
